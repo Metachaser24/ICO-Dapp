571f725498a42d31c078815ea54cedf6c80a6d0b
Metachaser24 <elomukoro@gmail.com>
2022-11-12 13:49:33 -0600

    initial commit

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+
+  const cryptoDevsTokenContract = await ethers.getContractFactory(
+    "CryptoDevToken"
+  );
+
+  const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
+    cryptoDevsNFTContract
+  );
+
+  console.log("Waiting on contract to deploy");
+
+  await deployedCryptoDevsTokenContract.deployed();
+
+  console.log(
+    "Crypto Devs Token Contract Address:",
+    deployedCryptoDevsTokenContract.address
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
